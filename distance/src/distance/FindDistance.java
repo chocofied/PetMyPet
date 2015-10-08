64326d3351c08f92804db4b39f4256fcc4ea58ae
aliciax <aliciaxu@yahoo.com>
2015-10-08 14:45:09 -0700

    LatLng added

--- distance/src/distance/FindDistance.java
+++ distance/src/distance/FindDistance.java
@@ -4,13 +4,22 @@ import java.net.*;
 import java.io.*;
 
 public class FindDistance {
+	//Change the API_KEY to your own, if you don't and keep using this one, it'll eventually
+	//or might max out on you
+	//This file should have eventually be connected to the LatLng one to get Origin and Destination
 	private static String API_KEY = "AIzaSyC2lK-ksS3EzEJEd7ALJVOG0sN-TQsGhto";
 	private static String BASE_URL = "https://maps.googleapis.com/maps/api/distancematrix/json?";
 	private static String ORIGIN = "7011 Sherbourne Lane, San Diego, CA";
 	private static String DESTINATION = "11108 Adobe Circle, IRVINE,CA";
 
 	public static void main(String args[]) throws IOException {
-		double kmToM = 0.621371;
+		double kmToM = 0.621371; 
+		//URL's are annoying in the sense that encoding screws up commas so the string was split
+		//but the part inside still needs to be encoded for the space, feel free to try out if it would work 
+		//without a space between each word in the URL
+		//this is by no way the cleanest way to do this but at the same time I can't remember exactly
+		//how to make functions in Java at this particular moment. Also in this case I would have ended up
+		//with potentially two for loops. It probably would be useful to clean and reuse the adding part
 		String[] originSplit = ORIGIN.split(",");
 		String[] destinationSplit = DESTINATION.split(",");
 		String ecodedURL = BASE_URL + "origins="
@@ -31,6 +40,8 @@ public class FindDistance {
 			double milesTemp;
 			String miles;
 			String temp;
+			//this is all about reading it in like .txt file
+			//most of this is converting km to miles as well manipulating strings to doubles and whatnot
 			while ((str = in.readLine()) != null) {
 				if (str.contains("text")) {
 					distance = str.trim().split(":")[1];
--- distance/src/distance/getAddress.java
+++ distance/src/distance/getAddress.java
@@ -7,16 +7,21 @@ import java.io.InputStreamReader;
 import java.net.URL;
 
 public class getAddress {
-	private static String API_KEY = "AIzaSyC2lK-ksS3EzEJEd7ALJVOG0sN-TQsGhto";
-	private static String BASE_URL = "https://maps.googleapis.com/maps/api/geocode/json?";
-	private static String LAT = "33.646600";
+	private static String API_KEY = "AIzaSyC2lK-ksS3EzEJEd7ALJVOG0sN-TQsGhto"; //Replace with your own API_KEY from Google developers
+	private static String BASE_URL = "https://maps.googleapis.com/maps/api/geocode/json?"; //first part of URL you need. They'll have a reference for you in API
+	//my temp values, this leads to my place in Irvine 
+	private static String LAT = "33.646600"; 
 	private static String LNG = "-117.834397";
 	
 	
 	public static void main(String args[]) throws IOException{
+		//This format will change depending on what you're doing realistically you should have 
+		//encoding do this but Java gave me a headache
 		String combinedURL = BASE_URL + "latlng=" + LAT + ","+ LNG + "&key=" + API_KEY;
-		System.out.println(combinedURL);
+		System.out.println(combinedURL); //just to print out url and check
 		URL url = new URL(combinedURL);
+		//this is all to read the JSON from the URL, it's more like a .txt file in this case
+		//you can try to figure out how to create a JSONObject and using the keys
 		try {
 			BufferedReader in = new BufferedReader(new InputStreamReader(
 					url.openStream(), "UTF-8"));
